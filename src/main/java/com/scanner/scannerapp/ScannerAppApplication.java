9bc5138664884bc4cbbc7eb5d2150ee9ab530c56
shishmis <shishir.m.mishra@oracle.com>
2025-04-05 13:07:53 +0530

    Shishir | Bug Fix in tomcat deployment

--- src/main/java/com/scanner/scannerapp/ScannerAppApplication.java
+++ src/main/java/com/scanner/scannerapp/ScannerAppApplication.java
@@ -2,12 +2,18 @@ package com.scanner.scannerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScannerAppApplication {
+public class ScannerAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ScannerAppApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScannerAppApplication.class, args);
     }
-
 }
